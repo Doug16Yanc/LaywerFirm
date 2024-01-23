81508623d6e4487b2e29f657f3f182b43347dfe1
Douglas Holanda <douglasholanda3195@gmail.com>
2024-01-22 22:03:23 -0300

    First interaction, enum and util

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import utilities.Utility;
+
 public class Program {
     public static void main(String[] args) {
-
+        doFirstInteraction();
+    }
+    public static void doFirstInteraction(){
+        Utility.printMessage("Welcome to lawyer firm.\n");
     }
 }
---
+++ src/utilities/Utility.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class Utility {
+    public static void printMessage(String message){
+        System.out.println("---------------------------------------------------\n");
+        System.out.println(message);
+        System.out.println("---------------------------------------------------\n");
+    }
+}
